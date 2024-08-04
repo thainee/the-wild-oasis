a98e514a9a11e0cdee81b863dcf79f0617053e10
thainee <thaivh9@fpt.com>
2024-08-04 17:14:47 +0700

    style(styled-components): setting up styles-components, global styles with styles components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert('Check in')}>Check in</Button>
+        <Button onClick={() => alert('Check out')}>Check out</Button>
+
+        <Input type='number' placeholder='Number of guest' />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
