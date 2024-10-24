c4ecb524234e5bc6d99fb0a88c95c4843b88efbd
thainee <thaivohong2003@gmail.com>
2024-10-24 17:46:05 +0700

    feat (bookings): api-side filtering: filtering bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filteredValue = searchParams.get('status') || 'all';
+  const filter =
+    filteredValue === 'all'
+      ? null
+      : { field: 'status', value: filteredValue, method: 'eq' };
+
   const {
     isPending,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isPending, error, bookings };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,17 @@
 import { getToday } from '../utils/helpers';
 import supabase from './supabase';
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from('bookings')
     .select(
       'id, createdAt, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)'
     );
+
+  // FILTER
+  if (filter) query[filter.method](filter.field, filter.value);
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
