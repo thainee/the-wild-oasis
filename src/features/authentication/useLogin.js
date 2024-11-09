966d405181f46301b2103b00ce8b28ecac5a6cc8
thainee <thaivohong2003@gmail.com>
2024-11-10 00:53:10 +0700

    fix (login): save the user object in cache

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -9,8 +9,8 @@ export function useLogin() {
 
   const { mutate: login, isPending } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: (user) => {
-      queryClient.setQueryData(['user'], user);
+    onSuccess: (data) => {
+      queryClient.setQueryData(['user'], data.user);
       navigate('/dashboard', { replace: true });
     },
 
