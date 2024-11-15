2b4311c896b9c366ab9e7aa59658e2962cf92665
thainee <thaivohong2003@gmail.com>
2024-11-15 20:26:11 +0700

    feat (dark mode): implementing dark mode with css variables

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import PageNotFound from './pages/PageNotFound';
 import Booking from './pages/Booking';
 import Checkin from './pages/Checkin';
 import ProtectedRoute from './ui/ProtectedRoute';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,56 +28,58 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
+    <DarkModeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
 
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route
-            element={
-              <ProtectedRoute>
-                <AppLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Navigate replace to='dashboard' />} />
-            <Route path='/dashboard' element={<DashBoard />} />
-            <Route path='/bookings' element={<Bookings />} />
-            <Route path='/bookings/:bookingId' element={<Booking />} />
-            <Route path='/checkin/:bookingId' element={<Checkin />} />
-            <Route path='/cabins' element={<Cabins />} />
-            <Route path='/users' element={<Users />} />
-            <Route path='/settings' element={<Settings />} />
-            <Route path='/account' element={<Account />} />
-          </Route>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate replace to='dashboard' />} />
+              <Route path='/dashboard' element={<DashBoard />} />
+              <Route path='/bookings' element={<Bookings />} />
+              <Route path='/bookings/:bookingId' element={<Booking />} />
+              <Route path='/checkin/:bookingId' element={<Checkin />} />
+              <Route path='/cabins' element={<Cabins />} />
+              <Route path='/users' element={<Users />} />
+              <Route path='/settings' element={<Settings />} />
+              <Route path='/account' element={<Account />} />
+            </Route>
 
-          <Route path='/login' element={<Login />} />
-          <Route path='*' element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
 
-      <Toaster
-        position='top-center'
-        gutter={12}
-        containerStyle={{ margin: '0.8rem' }}
-        toastOptions={{
-          success: {
-            duration: 3000,
-          },
-          error: {
-            duration: 5000,
-          },
-          style: {
-            fontSize: '1.6rem',
-            maxWidth: '50rem',
-            padding: '1.6rem 2.4rem',
-            backgroundColor: 'var(--color-grey-0)',
-            color: 'var(--color-grey-700)',
-          },
-        }}
-      />
-    </QueryClientProvider>
+        <Toaster
+          position='top-center'
+          gutter={12}
+          containerStyle={{ margin: '0.8rem' }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: '1.6rem',
+              maxWidth: '50rem',
+              padding: '1.6rem 2.4rem',
+              backgroundColor: 'var(--color-grey-0)',
+              color: 'var(--color-grey-700)',
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </DarkModeProvider>
   );
 }
 
---
+++ src/ui/DarkModeToggle.jsx
@@ -0,0 +1,15 @@
+import { HiOutlineMoon, HiOutlineSun } from 'react-icons/hi2';
+import ButtonIcon from './ButtonIcon';
+import { useDarkMode } from '../context/DarkModeContext';
+
+function DarkModeToggle() {
+  const { isDarkMode, handleToggleDarkMode } = useDarkMode();
+
+  return (
+    <ButtonIcon onClick={handleToggleDarkMode}>
+      {isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
+    </ButtonIcon>
+  );
+}
+
+export default DarkModeToggle;
